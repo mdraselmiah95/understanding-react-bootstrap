24ab127cf17ce1d45158abf69fee1180d7d5e133
rasel <russelroy15@gmail.com>
2021-09-28 00:38:33 +0600

    WIP on boostrap react

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const Body = () => {
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+      <Card />
+    </div>
+  );
+};
+
+export default Body;
